c616e057c55fbe6c7df9b5fd07a8a8623aa52552
Marwanishere <marwan.hasan.work@gmail.com>
2023-12-05 16:05:54 +0000

    using browser console log to get feedback and trying to set up send_email function

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -15,11 +15,15 @@ function compose_email() {
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
+  document.querySelector('#compose-form').addEventListener("submit", function(occurance){occurance.preventDefault(); send_email();});
+  // the above line adds an event listener to the submit button so that when it gets clicked, the default response is 
+  // overwritten and instead the function send_email is imposed.
 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+  document.querySelector("#emails-view").innerHTML = "";
 }
 
 function load_mailbox(mailbox) {
@@ -31,3 +35,24 @@ function load_mailbox(mailbox) {
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 }
+
+function send_email() {
+  console.log("if this message appears send_email is being called"),
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+      // document.querySelector('form').elements.recipient.value part of following lines
+      // adopted from cs50 chatbot
+      recipients: document.querySelector('form').elements.recipient.value,
+      subject: document.querySelector('form').elements.subject.value,
+      body: document.querySelector('form').elements.body.value
+    })
+  })
+  // res is short for response in js
+  .then(res => res.json())
+  .then(result => {if (result.status === "pass"){ console.log("passed in send_email");}})
+
+  // err is short for error
+  .catch(err => console.log('Error: Please try again', err));
+}
+
